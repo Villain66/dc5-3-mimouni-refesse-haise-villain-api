81548ebef8f1ecf682273d273f709777d5021a22
Villain66 <t.villainpro@gmail.com>
2023-11-28 11:00:38 +0100

    ajout server.js

---
+++ back/routes/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const routes = require('./routes');
+const app = express();
+const port = 3000;
+app.use(bodyParser.json());
+app.use(bodyParser.json());
+//app.use(routes);
+app.listen(port, () => {
+    console.log(`Le serveur tourne sur le port : ${port}`);
+});
